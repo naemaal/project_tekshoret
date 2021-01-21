7103143c633edf26bfae3bfef5acaab0ad1199f6
naemaal <75208493+naemaal@users.noreply.github.com>
2021-01-21 20:51:31 +0200

    Add files via upload

---
+++ DAL/Dal.cs
@@ -0,0 +1,31 @@
+﻿using MoviesBooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MoviesBooking.DAL
+{
+    public class Dal:DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("usersTbl");
+            modelBuilder.Entity<Movie>().ToTable("moviesTbl");
+            modelBuilder.Entity<Ticket>().ToTable("ticketsTbl");
+            modelBuilder.Entity<Hall>().ToTable("hallsTbl");
+            modelBuilder.Entity<Guest>().ToTable("GuestTbl");
+
+        }
+        public DbSet<User> users { get; set; }
+        public DbSet<Movie> movies { get; set; }
+        public DbSet<Ticket> tickets { get; set; }
+        public DbSet<Hall> halls { get; set; }
+        public DbSet<Guest> guests { get; set; }
+
+
+    }
+
+}
